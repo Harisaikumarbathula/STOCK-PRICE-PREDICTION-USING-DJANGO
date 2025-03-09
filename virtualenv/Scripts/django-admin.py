c76214a65a680c550b416770d1aaa3a6957e11bd
Harisaikumarbathula <harisaikumar745@gmail.com>
2025-03-09 10:25:54 +0530

    #

--- virtualenv/Scripts/django-admin.py
+++ virtualenv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!e:\final year project\virtualenv\scripts\python.exe
+#!e:\final-year-project\virtualenv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
